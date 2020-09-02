56df5271fcdeae7b1c54fe4c72225b54c38d426f
Christabel Escarez <christabel.escarez@gmail.com>
2020-09-01 20:48:29 -0700

    Implemented enhancements, with error-checking.

--- random_menu.rb
+++ random_menu.rb
@@ -3,20 +3,36 @@
 #Ada Developer's Academy C14
 #Ada Assignment, "Random Menu": https://github.com/Ada-C14/random-menu
 #sources: https://ruby-doc.org/core-2.7.1/Array.html
-#Future modifications: (1) no repeat pulls from arrays when creating menu items (2) user-determined # of menu items < smallest array (3) user-input arrays.
+#Future modifications: (1) improve the input check for nil.integers (2) require > 0 inputs for each array.
 
 #Methods
 def build_array(array)
-  entry = ""
-  until entry == 'q'
-    entry = gets.chomp.downcase
-    if entry != 'q'
+  entry = nil
+  until entry == 'Q' && array.length > 0
+    entry = gets.chomp.upcase
+    #check for empty inputs, or integer inputs
+    while entry.strip == "" || entry.to_i != 0 || entry.to_s == '0'
+      puts "Invalid input. Please reenter."
+      entry = gets.chomp.upcase
+    end
+    #execute if no exit flag
+    if entry != 'Q'
       if entry && !array.include?(entry)
         array << entry
       end
       puts "#{entry} has been added to the list. Please add another entry, or press 'q' to proceed."
     end
+    #if exit flag, but array is empty, prompt for additional input
+    if array.length == 0
+      puts "You must input at least one entry."
+    end
   end
+end
+
+def extract_element(array)
+  element = array.sample
+  array.delete(element)
+  return element
 end
 
 #MAIN
@@ -34,28 +50,25 @@ nouns = []
 puts "Please enter a noun: "
 build_array(nouns)
 
-# if adjectives.length <= verbs.length && adjectives.length <= nouns.length
-#   max_menu_items = adjectives.length
-# elsif verbs.length <= adjectives.length && verbs.length <= nouns.length
-#   max_menu_items = verbs.length
-# else
-#   max_menu_items = nouns.length
-# end
-
 max_menu_items = [adjectives.length, verbs.length, nouns.length].min
 
-menu_items = []
+num_items = 0
 print "How many menu items would you like to see today? "
-num_items = nil
-until num_items
+until (num_items > 0) && (num_items <= max_menu_items)
   num_items = gets.chomp.to_i
-  if num_items > max_menu_items
-    puts "Please enter a number less than #{max_menu_items}."
+  while num_items > max_menu_items
+    print "Please enter a number less than #{max_menu_items + 1}. "
+    num_items = gets.chomp.to_i
   end
 end
 
+menu_items = []
+puts "Today's menu at Maison de Mad Libs is: "
 num_items.times do |i|
-  menu_item = adjectives.sample + " " + verbs.sample + "-" + nouns.sample
+  adjective = extract_element(adjectives)
+  verb = extract_element(verbs)
+  noun = extract_element(nouns)
+  menu_item = adjective + " " + verb + " " + noun
   menu_items << menu_item
   puts (i + 1).to_s + ". " + menu_item
 end
