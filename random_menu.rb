444c13b37836d4f48e035b68eeb341634a35ad01
Christabel Escarez <christabel.escarez@gmail.com>
2020-09-01 18:55:22 -0700

    Added future modifications to header comment.

--- random_menu.rb
+++ random_menu.rb
@@ -3,6 +3,7 @@
 #Ada Developer's Academy C14
 #Ada Assignment, "Random Menu": https://github.com/Ada-C14/random-menu
 #sources: https://ruby-doc.org/core-2.7.1/Array.html
+#Future modifications: (1) no repeat pulls from arrays when creating menu items (2) user-determined # of menu items < smallest array (3) user-input arrays.
 
 
 systems_adjectives = ["Free-range", "Organic", "GMO-free", "Gluten-free", "Fair-trade", "Sugar-free", "Vegan", "Local", "In-season", "Keto-friendly"]
