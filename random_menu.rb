ab53b78b28d77bd46bd1f4bf28fcaa266ca2b72b
Christabel Escarez <christabel.escarez@gmail.com>
2020-09-01 19:21:37 -0700

    Create "optional" git branch to implement optional enhancements.

--- random_menu.rb
+++ random_menu.rb
@@ -5,14 +5,34 @@
 #sources: https://ruby-doc.org/core-2.7.1/Array.html
 #Future modifications: (1) no repeat pulls from arrays when creating menu items (2) user-determined # of menu items < smallest array (3) user-input arrays.
 
+#Methods
+def check_input(user_input)
+  until user_input == 'y' || user_input == 'n'
+    user_input = gets.chomp.downcase
+  end
+  return user_input
+end
 
-systems_adjectives = ["Free-range", "Organic", "GMO-free", "Gluten-free", "Fair-trade", "Sugar-free", "Vegan", "Local", "In-season", "Keto-friendly"]
-asian_foods = ["rice", "noodles", "bao", "curry", "egg-drop soup", "banh mi", "arroz caldo", "pad see ew", "dinuguan", "sushi"]
-desserts= ["pie", "cake", "cupcake", "lollipop", "pudding", "ice cream", "cookie", "flan", "brownies", "cakepop"]
+#MAIN
+adjectives = []
+verbs = []
+nouns = []
+
+
+adjectives = []
+puts "Welcome to Maison de Mad-Libs! Please enter an adjective: "
+until adjective == 'q'
+  adjective = gets.chomp.downcase
+  if adjective != 'q'
+    if adjective && !adjectives.include?(adjective)
+      adjectives << adjective
+    end
+    puts "#{adjective} has been added to the list. Please add another adjective, or press 'q' to proceed."
+  end
+end
 
 menu_items = Array.new
-
-print "Welcome to PDX Savory-Desserts! How many menu items would you like to see today? "
+print "How many menu items would you like to see today? "
 num_items = nil
 until num_items
   num_items = gets.chomp.to_i
